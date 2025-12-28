999b58a58d280a3ed7113209f03a3167234aa9f4
momo-694 <romelestoque3@gmail.com>
2025-12-28 11:22:30 +0900

    Updated Names

--- Apol/src/game/screen/MainFrame.java
+++ Apol/src/game/screen/MainFrame.java
@@ -5,11 +5,11 @@ import javax.swing.JFrame;
 public class MainFrame extends JFrame {
     public MainFrame() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("Random Shi");
+        setTitle("2D Game");
         add(new MainPanel());
         pack();
         setLocationRelativeTo(null);
-         setResizable(false);
+        setResizable(false);
 
         setVisible(true);
     }
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Random-Shi
-None
+# IN PROGRESS...
